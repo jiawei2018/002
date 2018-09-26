586a15e856d75b3b5abb079402b8494361a16ac7
jiawei2018 <35362950+jiawei2018@users.noreply.github.com>
2018-09-26 16:43:51 -0700

    Initial commit

---
+++ src/a009/populatingNextRightPointersInEachNode_II_117.java
@@ -0,0 +1,49 @@
+package a009;
+
+/**
+ * 117. Populating Next Right Pointers in Each Node II
+ * DescriptionHintsSubmissionsDiscussSolution
+ * Pick One
+ * Given a binary tree
+ *
+ * struct TreeLinkNode {
+ *   TreeLinkNode *left;
+ *   TreeLinkNode *right;
+ *   TreeLinkNode *next;
+ * }
+ * Populate each next pointer to point to its next right node.
+ * If there is no next right node, the next pointer should be set to NULL.
+ *
+ * Initially, all next pointers are set to NULL.
+ *
+ * Note:
+ *
+ * You may only use constant extra space.
+ * Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+ * Example:
+ *
+ * Given the following binary tree,
+ *
+ *      1
+ *    /  \
+ *   2    3
+ *  / \    \
+ * 4   5    7
+ * After calling your function, the tree should look like:
+ *
+ *      1 -> NULL
+ *    /  \
+ *   2 -> 3 -> NULL
+ *  / \    \
+ * 4-> 5 -> 7 -> NULL
+ */
+public class populatingNextRightPointersInEachNode_II_117 {
+   class TreeLinkNode {
+      int val;
+      TreeLinkNode left, right, next;
+      TreeLinkNode(int x) { val = x; }
+  }
+        public void connect(TreeLinkNode root) {
+
+        }
+}
